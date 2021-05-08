d12e8765ce3a50804f9dbfc528fc7d0585f1e6bf
devanshgoyal25 <dgoyal@haverford.edu>
2021-05-08 21:26:48 +0530

    Adding starter files for activities

--- app/src/main/java/com/example/happyalpacas/MainActivity.java
+++ app/src/main/java/com/example/happyalpacas/MainActivity.java
@@ -1,16 +1,48 @@
 package com.example.happyalpacas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Trying git
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        //TODO Implement action to home
+                        break;
+                    case R.id.action_search:
+                        //TODO Implement action to search
+                        break;
+                    case R.id.action_post:
+                        //TODO Implement action to post
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        //TODO Implement action to profile
+                        break;
+                }
+              return true;
+            }
+        });
+
     }
 }
